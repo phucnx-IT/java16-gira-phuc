f571da48bf688a63abc0fdbeaa32f5cc2487699f
phucnx-IT <phucnx.phar@gmail.com>
2022-03-03 21:40:40 +0700

    third commit

--- src/main/java/cybersoft/javabackend/java16giraphuc/config/SwaggerConfig.java
+++ src/main/java/cybersoft/javabackend/java16giraphuc/config/SwaggerConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.RequestHandledEvent;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,6 +18,10 @@ public class SwaggerConfig {
 	public Docket getDocket() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("cybersoft.javabackend.java16giraphuc"))
-						.build();
+						.build().apiInfo(new ApiInfoBuilder().title("Gira Application")
+								.version("1.0.0")
+								.description("This project is used for education purpose only.")
+								.contact(new Contact("Xuan Phuc", "https://phucnx.dev/", "phucnx.phar@gmail.com"))
+								.build());
 	}
 }
